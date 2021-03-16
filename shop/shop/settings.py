dbeeb5b6258da47cd402962235d923bcc0067e8a
dheerukreddy <60852451+Kdheeru12@users.noreply.github.com>
2021-03-17 01:39:38 +0530

    h

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -136,13 +136,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_HOST_USER = 'noreply.blozz@gmail.com'
-
-EMAIL_HOST_PASSWORD = 'Kmunna@!22001'
-
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'yagneshreddyjeemains2018@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'kyr2nd@gmail.com'
+
 
 AUTH_USER_MODEL = 'shop1.ExtendUser'
 
--- shop/shop1/views.py
+++ shop/shop1/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render,HttpResponse
-
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 def hello(request):
-    return HttpResponse('hdddd')
+    send_mail(
+    'Regarding Login Into the WEBSITE',
+    'testing',
+    settings.EMAIL_HOST,
+    ['dheerukreddy@gmail.com'],
+    )
+    # import smtplib 
+    
+    # # creates SMTP session 
+    # s = smtplib.SMTP('smtp.gmail.com', 587) 
+    
+    # # start TLS for security 
+    # s.starttls() 
+    
+    # # Authentication 
+    # s.login("dheerukreddy@gmail.com", "Kmunna@!22001") 
+    
+    # # message to be sent 
+    # message = "Message_you_need_to_send"
+    
+    # # sending the mail 
+    # s.sendmail("dheerukreddy@gmail.com", "dheerukreddy@gmail.com", message) 
+    
+    # # terminating the session 
+    # s.quit()
+
+    return HttpResponse('hdddd') 
