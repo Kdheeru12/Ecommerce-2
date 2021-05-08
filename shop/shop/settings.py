9aeec8f4489644f1614bb06ac3cfbcd639457f8b
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2021-05-08 20:48:59 +0530

    procfile

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,9 +138,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
 STATIC_ROOT = '/static/'
-# STATICFILES_DIRS = (
-#  os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+ os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
 
